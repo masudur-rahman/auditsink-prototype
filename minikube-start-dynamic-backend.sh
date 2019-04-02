d63e841b04e444d4598eb8f367956d9cf713898a
Masudur Rahman <masud@appscode.com>
2019-04-02 16:10:19 +0600

    Update minikube-start-dynamic-backend.sh

--- minikube-start-dynamic-backend.sh
+++ minikube-start-dynamic-backend.sh
@@ -6,5 +6,4 @@ minikube start \
   --kubernetes-version=v1.14.0 \
   --extra-config=apiserver.audit-dynamic-configuration=true \
   --extra-config=apiserver.feature-gates=DynamicAuditing=true \
-  --extra-config=apiserver.runtime-config=auditregistration.k8s.io/v1alpha1=true \
-
+  --extra-config=apiserver.runtime-config=auditregistration.k8s.io/v1alpha1=true
